afbdf56c9d0b41c819071f44a91494dda60ba837
heng <hengfun@gmail.com>
2019-05-01 17:36:52 +0200

    added intializer

--- seq2seq.py
+++ seq2seq.py
@@ -9,14 +9,38 @@ class Seq2seq(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.layers = layers
+        self.initializer_hh = torch.nn.init.orthogonal_
+        self.initializer_ih = torch.nn.init.xavier_normal_
+        # self.forget_bias = torch.tensor(2.0,requires_grad=True).float()
+        self.forget_bias =2.0
         self.build_model(rnn)
+        
+        
+
     def build_model(self,rnn):
         if rnn == 'LSTM':
-            self.encoder = nn.LSTM(self.input_size,self.hidden_size,self.layers, batch_first=True)
-            self.decoder = nn.LSTM(self.input_size,self.hidden_size,self.layers, batch_first=True)
+            self.encoder = self.init_lstm()
+            self.decoder = self.init_lstm()
         if rnn == 'GRU':
             self.encoder = nn.GRU(self.input_size,self.hidden_size,self.layers, batch_first=True)
             self.decoder = nn.GRU(self.input_size,self.hidden_size,self.layers, batch_first=True)
+
+    def init_lstm(self):
+        lstm = nn.LSTM(self.input_size,self.hidden_size,self.layers, batch_first=True)
+        for name,weights in lstm.named_parameters():
+            # print(name,weights)
+            if "bias_hh" in name:
+                #weights are initialized 
+                #(b_hi|b_hf|b_hg|b_ho), 
+                weights[self.hidden_size:self.hidden_size].data.fill_(self.forget_bias)
+            elif 'bias_ih' in name:
+                 #(b_ii|b_if|b_ig|b_io)
+                pass
+            elif "weight_hh" in name:
+                weights = self.initializer_hh(weights)
+            elif 'weight_ih' in name:
+                weights = self.initializer_ih(weights)
+        return lstm
 
     def forward(self,inputs):
         enc_out, enc_state = self.encoder(inputs)
